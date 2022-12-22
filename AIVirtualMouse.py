ad6edc4f3a94bd082fd4f0099aef33745291ec5d
MLR-git <103881725+MLR-git@users.noreply.github.com>
2022-12-22 12:00:32 -0500

    Update AIVirtualMouse.py

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -23,45 +23,45 @@ cX = 0
 cY = 0
 
 while True:
-    #1. Find hand landmarks
+    #Find hand landmarks
     success, img = capture.read()
     img = detector.findHands(img)
     landmarkList = detector.findPosition(img)
 
-    #2. Get index & middle fingers
+    #Get index & middle fingers
     if len(landmarkList) != 0:
         #index finger coordinates (x/y1)
         x1, y1 = landmarkList[8][1:]
         #middle finger coordinates (x/y2)
         x2, y2 = landmarkList[12][1:]
 
-        #3. Check which fingers are up
+        #Check which fingers are up
         fingerTips = detector.fingersUp()
         cv2.rectangle(img, (frame, frame), (wDefault - frame, hDefault - frame), (255, 0, 255), 2)
 
-        #4. Index finger only = movement mode
+        #Index finger only = movement mode
         if fingerTips[1] == 1 and fingerTips[2] == 0:
-            #4a. Convert coordinates from default resolution to actual resolution
+            #Convert coordinates from default resolution to actual resolution
             x3 = numpy.interp(x1, (frame, wDefault - frame), (0, wScreen)) #convert x1 from the first range to the second
             y3 = numpy.interp(y1, (frame, hDefault - frame), (0, hScreen))
-            #4b. Smooth values
+            #Smooth values
             cX = pX + (x3 - pX)/smooth
             cY = pY + (y3 - pY)/smooth
-            #4c. Move mouse
+            #Move mouse
             autopy.mouse.move(wScreen - cX, cY)
             cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
             pX = cX
             pY = cY
 
-    #5. Index + Middle fingers = clicking mode
-        #5a. Find distance b/w fingers
+    #Index + Middle fingers = clicking mode
+        #Find distance b/w fingers
         if fingerTips[1] == 1 and fingerTips[2] == 1:
             length, img, linePoints = detector.findDistance(8, 12, img)
-            # 5b. Click mouse if distance is short
+            #Click mouse if distance is short
             if(length < 32):
                 cv2.circle(img, (linePoints[4], linePoints[5]), 15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
 
-    #6. Display
+    #Display
     cv2.imshow("Image", img)
     cv2.waitKey(30)
